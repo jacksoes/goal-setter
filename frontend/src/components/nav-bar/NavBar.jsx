b8848d83bb8cd1a04b6b1bd68b1ac4c3976a0a2f
jack soesman <jsoesman1@yahoo.com>
2024-08-04 18:30:52 -0400

    complete navbar design

---
+++ frontend/src/components/nav-bar/NavBar.jsx
@@ -0,0 +1,35 @@
+import "./NavBar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Goal-Setter</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto to-front">
+            <Nav.Link href="#link">Log In</Nav.Link>
+            <NavDropdown title="Goals" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Short-term Goals</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Long-term Goals
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">All Goals</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Data
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+
+export default NavBar;
