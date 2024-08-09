1101c98530e8e729bf95a1f31fa964e16a5ca1c9
jack soesman <jsoesman1@yahoo.com>
2024-08-09 10:41:20 -0400

    create post request for frontend logIn form

--- frontend/src/components/login-form/LogInForm.tsx
+++ frontend/src/components/login-form/LogInForm.tsx
@@ -1,21 +1,45 @@
-import "./LogInForm.css"
+import "./LogInForm.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 
-
 const LogInForm = () => {
+  const submitLogIn = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/logIn", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userName: event.target[0].value,
+        password: event.target[1].value,
+      }),
+    })
+      .then((response) => response)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <Container className="responsive-container-small d-flex justify-content-center">
-      <Form className="mt-3">
+      <Form onSubmit={submitLogIn} className="mt-3">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" autoComplete="email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            autoComplete="email"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" autoComplete="password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            autoComplete="password"
+          />
         </Form.Group>
         <Button className="w-100 mb-3 blue-button" type="submit">
           Log In
