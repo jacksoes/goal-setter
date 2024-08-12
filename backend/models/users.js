16c3f06f6087c84302e872688b78908922a6526b
jack soesman <jsoesman1@yahoo.com>
2024-08-12 11:16:11 -0400

    create basic user schema with mongoose

---
+++ backend/models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+        
+    }
+    
+    password: {
+        type: String,
+        required: true,
+        trim: true 
+    }
+})
+
+module.exports = mongoose.model('user', userSchema);
