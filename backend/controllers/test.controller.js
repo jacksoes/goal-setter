8ceb267580807509dd2491d6fa2e04a115e42495
jack soesman <jsoesman1@yahoo.com>
2024-08-29 10:28:11 -0400

    remove un-needed sessions model

--- backend/controllers/test.controller.js
+++ backend/controllers/test.controller.js
@@ -1,11 +1,5 @@
-const sessions = require("../models/session.js");
 
 const testControllerGet = (req, res) => {
-    console.log(req.cookies);
-    console.log("test user authenticated", req.isAuthenticated());
-    console.log(sessions.find());
-
-    res.json({"message": 'testController: res sent'});
 
 }
 
