a296f1b21c0bf6bd93517ba1ecc2f02b741b7522
jack soesman <jsoesman1@yahoo.com>
2025-03-20 18:14:31 -0400

    fix date string displying invalid date bug

--- frontend/src/components/goals-card/GoalsCard.tsx
+++ frontend/src/components/goals-card/GoalsCard.tsx
@@ -95,7 +95,7 @@ const GoalsCard: React.FC<cardProps> = ({
 
   let dateString = "";
 
-  if (date instanceof Date && date != new Date("12-32-1969") || isNaN(date.getFullYear()) ) {
+  if (date instanceof Date && !isNaN(new Date(date).getTime()) ) {
     dateString =
       date.getMonth() +
       1 +
